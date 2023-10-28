1abf85d8f0cf3e38f34b3c02413e1720263e7a98
ayfersarikoc <ayfersarikoc76@gmail.com>
2023-10-28 17:47:41 +0100

    ayfer 28/10/2023 configreader dosya yolu degistirildi

---
+++ src/test/java/tests/Ayfer/deneme.java
@@ -0,0 +1,9 @@
+package tests.Ayfer;
+
+import utilities.ConfigReader;
+
+public class deneme {
+    public static void main(String[] args) {
+        System.out.println(ConfigReader.getProperty("user_username"));
+    }
+}
