d240fedb1851dd3f7d1b0b0579895bb5a64b995f
ayfersarikoc <ayfersarikoc76@gmail.com>
2023-10-29 23:47:18 +0000

    ayfer 29/10/2023 26,27 ve 29.user story ler bitti

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,10 +1,16 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
 public class HomePage {
 public  HomePage(){PageFactory.initElements(Driver.getDriver(),this);}
+
+
+    @FindBy(xpath = "//a[@class='btn btn-white fs-18 ms-3 d-lg-block d-none']")//home page/header daki sign in butonu
+    public WebElement signinButton;
 
 
 }
