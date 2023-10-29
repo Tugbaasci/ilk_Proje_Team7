e04f61ea127f2506b3ac072ccaeed1c3f9f2a9f9
Busra Dakak <busradakak@gmail.com>
2023-10-29 02:38:37 +0100

    busra kaydetti

--- src/test/java/tests/Busra/US_006.java
+++ src/test/java/tests/Busra/US_006.java
@@ -3,5 +3,7 @@ package tests.Busra;
 public class US_006 {
     public static void main(String[] args) {
         System.out.println("Good Luck Everyone");
+        System.out.println("deneme");
+
     }
 }
