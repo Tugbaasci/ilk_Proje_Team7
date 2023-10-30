ee92abe57772a362ab2ce8b604b583d1fc28ce51
bulent-1111 <akuklubulent@gmail.com>
2023-10-30 21:51:12 +0100

    bulent 30/10/2023 US_04

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.bouncycastle.jcajce.provider.asymmetric.rsa.ISOSignatureSpi;
+import org.checkerframework.framework.qual.QualifierForLiterals;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -11,6 +13,32 @@ public  HomePage(){PageFactory.initElements(Driver.getDriver(),this);}
 
     @FindBy(xpath = "//a[@class='btn btn-white fs-18 ms-3 d-lg-block d-none']")//home page/header daki sign in butonu
     public WebElement signinButton;
+
+    // bulent
+    @FindBy(xpath = "//a[@title='Facebook']")
+    public WebElement footerFacebookIcon;
+    @FindBy(xpath = "//a[@title='Twitter']")
+    public WebElement footerXIcon;
+    @FindBy(xpath = "//a[@title='Instagram']")
+    public WebElement footerInstagramIcon;
+    @FindBy(xpath = "//a[@title='Linkedin']")
+    public WebElement footerLinkedInIcon;
+    @FindBy(xpath = "//a[@title='Pinterest']")
+    public WebElement footerPinterestIcon;
+    @FindBy(linkText = "Terms & Conditions")
+    public WebElement footerTermsConditions;
+    @FindBy (linkText = "Privacy Policy")
+    public WebElement footerPrivacyPolicy;
+    @FindBy (linkText = "FAQ")
+    public WebElement footerFAQ;
+    @FindBy (xpath = "//h1[text()='Terms & Conditions']")
+    public WebElement termsConditionsWE;
+    @FindBy(xpath = "//h1[text()='Privacy Policy']")
+    public WebElement privacyPolicyWE;
+    @FindBy(xpath = "//h1[text()='Frequently Asked Questions (FAQ)']")
+    public WebElement faqWE;
+
+
 
 
 }
--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -66,4 +66,11 @@ public class Driver {
         }
     }
 
+    public static void quitDriver(){
+        if (driver != null){
+            driver.quit();
+            driver=null;
+        }
+    }
+
 }
